f6ef7bbe0fc1fbc5ce21024a09d70818453e9b0e
TassioSales <74218122+TassioSales@users.noreply.github.com>
2021-02-15 22:37:10 -0300

    Curso hashtag

---
+++ aula 6/main.py
@@ -0,0 +1,20 @@
+import pandas as pd
+# pandas
+# openpyxl
+# twilio
+
+# Passo a Passo Solução
+
+# Abrir os 6 arquivos em excel
+
+lista_meses = ["janeiro", "fevereiro", "março", "abril", "maio", "junho"]
+
+
+
+# Para cada arquivos
+
+# Verificar se algum valor na coluna vendas e maior que 55 mil
+
+# se for maior do que 55 mil -> Enviar SMS com o Nome, o mes e as Vendas dos vendedor
+
+# Caso não seja maior do que 55 mil não quero fazer nada
