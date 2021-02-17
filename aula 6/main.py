84244901ad79c3a388624a27c0011afeb7539a52
TassioSales <74218122+TassioSales@users.noreply.github.com>
2021-02-17 20:19:00 -0300

    projeto completo

--- aula 6/main.py
+++ aula 6/main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from twilio.rest import Client
+import openpyxl as xl
 # pandas
 # openpyxl
 # twilio
@@ -7,11 +9,29 @@ import pandas as pd
 
 # Abrir os 6 arquivos em excel
 
+# Your Account SID from twilio.com/console
+account_sid = "ACe555dadaced23bbf562b45654b2dcff8"
+# Your Auth Token from twilio.com/console
+auth_token = "51729746f3ce674818294072e4304863"
+client = Client(account_sid, auth_token)
+
 lista_meses = ["janeiro", "fevereiro", "março", "abril", "maio", "junho"]
 
+for mes in lista_meses:
+    tabela_vendas = pd.read_excel(f"{mes}.xlsx")
+    if (tabela_vendas["Vendas"] > 55000).any():
+        vendedor = tabela_vendas.loc[tabela_vendas["Vendas"] > 55000, 'Vendedor'].values[0]
+        venda = tabela_vendas.loc[tabela_vendas["Vendas"] > 55000, 'Vendas'].values[0]
+        message = client.messages.create(
+            to="+5561982970840",
+            from_="++13303022824",
+            body="Oi amor")
+        print(message.sid)
 
 
-# Para cada arquivos
+# Para cada arquivos:
+
+
 
 # Verificar se algum valor na coluna vendas e maior que 55 mil
 
